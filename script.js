8c8eda3696b79a6def06657d0bb0de5acde86ae5
Lucas Eni De Jesus <lucas.eni@outlook.com>
2022-05-08 19:30:44 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+function onScroll() {
+   if(scrollY > 0){
+       navigation.classList.add('scroll')
+   }else{
+       navigation.classList.remove('scroll')
+   }
+};
+function openMenu(){
+    document.body.classList.add('menu-expended')
+};
+function closeMenu(){
+    document.body.classList.remove('menu-expended')
+};
+ScrollReveal({
+    origin: 'top',
+    distance: '30rem',
+    duration: 800,
+}).reveal(`#home,
+ #home img,
+#home .stats,
+#services,
+#services header,
+#sercices .card,
+#about,
+#about header,
+#about .content`);
